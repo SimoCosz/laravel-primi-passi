52ff18c334ad5e5cbb15859201164c8529f68c66
coszach@hotmail.it <coszach@hotmail.it>
2022-04-11 14:48:03 +0200

    add 'Hello Word'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>laravel-primi-passi</title>
+</head>
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  img{
+    max-width: 100%;
+    display: block;
+  }
+
+  .container{
+    width: 1024px;
+    margin: 0 auto;
+  }
+
+  .title{
+    text-align: center;
+    color: #555555;
+    font-size: 60px;
+  }
+
+
+
+</style>
+<body>
+  <div class="container">
+    <h1 class="title">Hello Word</h1>
+
+  </div>
+</body>
+</html>
